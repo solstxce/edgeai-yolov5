3e8e169a33ba23d624b62aa8cd94b58dcdaf5740
solstxce <48374053+solstxce@users.noreply.github.com>
2024-12-09 00:11:04 +0530

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -463,13 +463,12 @@ def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
         gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
         pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
     else:
-        gain = ratio_pad[0]
+        gain = ratio_pad[0][0]
         pad = ratio_pad[1]
 
     coords[:, [0, 2]] -= pad[0]  # x padding
     coords[:, [1, 3]] -= pad[1]  # y padding
-    coords[:, [0, 2]] /= gain[1]
-    coords[:, [1, 3]] /= gain[0]
+    boxes[..., :4] /= gain
     clip_coords(coords, img0_shape)
     return coords
 
